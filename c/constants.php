284cbe87536c4a1b45d08eb7137be99ad27cc900
Adam Kiryk <akiryk@Adams-Macbook-Pro.local>
2015-08-28 15:01:31 -0400

    Second commit

--- c/constants.php
+++ c/constants.php
@@ -1,14 +1,5 @@
 <?php
  // Database Constants
-	/*
-	DEFINE("DB_SERVER", "mysql.storyengine.adamkiryk.com");
-	DEFINE("DB_USER", "akiryk");
-	DEFINE("DB_PASS", "1Forg3table");
-	DEFINE("DB_NAME", "ak_storyengine");
-*/
-?>
-<?php
- // Database Constants
 	DEFINE("DB_SERVER", "localhost");
 	DEFINE("DB_USER", "root");
 	DEFINE("DB_PASS", "root");
